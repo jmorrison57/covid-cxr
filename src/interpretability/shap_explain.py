cffdd0226d0c582ff430ec267e72d9905ae9719a
John Morrison <jmorrison5725@gmail.com>
2020-11-23 00:41:59 -0500

    epochs

--- src/interpretability/shap_explain.py
+++ src/interpretability/shap_explain.py
@@ -1,21 +1,19 @@
+from tensorflow.compat.v1.keras.backend import get_session
+import numpy as np
 import shap
-import pandas as pd
+import tensorflow.compat.v1.keras.backend as K
+import json
+import matplotlib.pyplot as pl
 import yaml
-import os
-import datetime
+import pandas as pd
 import dill
 import cv2
-import numpy as np
-from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-
+from tensorflow.keras.models import load_model
+from IPython.core.debugger import set_trace
 import sys, os
-sys.path.append(os.path.join(os.path.dirname(sys.path[0]),'../'))
-
-from src.visualization.visualize import visualize_explanation
-from src.predict import predict_instance, predict_and_explain
+sys.path.append(os.path.join(os.path.dirname(sys.path[0]),''))
 from src.data.preprocess import remove_text
-
 
 def setup_shap():
     '''
@@ -24,7 +22,7 @@ def setup_shap():
     '''
 
     # Load relevant constants from project config file
-    cfg = yaml.full_load(open(os.getcwd() + "/config.yml", 'r'))
+    cfg = yaml.full_load(open(os.getcwd()/ + "../config.yml", 'r'))
     shap_dict = {}
     shap_dict['NUM_SAMPLES'] = cfg['LIME']['NUM_SAMPLES']
     shap_dict['NUM_FEATURES'] = cfg['LIME']['NUM_FEATURES']
@@ -39,8 +37,11 @@ def setup_shap():
     FEATURE_SELECTION = cfg['LIME']['FEATURE_SELECTION']
 
     # Load train and test sets
-    shap_dict['TRAIN_SET'] = pd.read_csv(cfg['PATHS']['TRAIN_SET'])
-    shap_dict['TEST_SET'] = pd.read_csv(cfg['PATHS']['TEST_SET'])
+    try:
+      shap_dict['TRAIN_SET'] = pd.read_csv(cfg['PATHS']['TRAIN_SET'])
+      shap_dict['TEST_SET'] = pd.read_csv(cfg['PATHS']['TEST_SET'])
+    except:
+      print("issue")
 
     # Create ImageDataGenerator for test set
     test_img_gen = ImageDataGenerator(preprocessing_function=remove_text,
@@ -50,65 +51,66 @@ def setup_shap():
         class_mode='categorical', validate_filenames=False, shuffle=False)
     shap_dict['TEST_GENERATOR'] = test_generator
 
-    # Define the LIME explainer
-    shap_dict['EXPLAINER'] = LimeImageExplainer(kernel_width=KERNEL_WIDTH, feature_selection=FEATURE_SELECTION,
-                                                verbose=True)
-    dill.dump(shap_dict['EXPLAINER'], open(cfg['PATHS']['LIME_EXPLAINER'], 'wb'))    # Serialize the explainer
-
     # Load trained model's weights
     shap_dict['MODEL'] = load_model(cfg['PATHS']['MODEL_TO_LOAD'], compile=False)
 
     return shap_dict
 
-
-def explain_xray(shap_dict, idx, save_exp=True):
+def explain_xray_shap(shap_dict, idx, layerToExplain, save_exp=True):
     '''
     Make a prediction and provide a LIME explanation
-    :param shap_dict: dict containing important information and objects for explanation experiments
+    :param lime_dict: dict containing important information and objects for explanation experiments
     :param idx: index of image in test set to explain
     :param save_exp: Boolean indicating whether to save the explanation visualization
     '''
 
-    # Get i'th preprocessed image in test set
+    # Get 50 preprocessed image in test set
     shap_dict['TEST_GENERATOR'].reset()
-    for i in range(idx + 1):
-        x, y = shap_dict['TEST_GENERATOR'].next()
-    x = np.squeeze(x, axis=0)
+    X = []
+    y = []
+    for i in range(50):
+        x, o = shap_dict['TEST_GENERATOR'].next()
+        x = np.squeeze(x, axis=0)
+        X.append(x)
+        y.append(o)
+    #x = np.squeeze(x, axis=0)
+    X = np.array(X)
+    y = np.array(y)
 
     # Get the corresponding original image (no preprocessing)
     orig_img = cv2.imread(shap_dict['RAW_DATA_PATH'] + shap_dict['TEST_SET']['filename'][idx])
     new_dim = tuple(shap_dict['IMG_DIM'])
     orig_img = cv2.resize(orig_img, new_dim, interpolation=cv2.INTER_NEAREST)     # Resize image
 
-    # Make a prediction for this image and retrieve a LIME explanation for the prediction
-    start_time = datetime.datetime.now()
-    explanation, probs = predict_and_explain(x, shap_dict['MODEL'], shap_dict['EXPLAINER'],
-                                      shap_dict['NUM_FEATURES'], shap_dict['NUM_SAMPLES'])
-    print("Explanation time = " + str((datetime.datetime.now() - start_time).total_seconds()) + " seconds")
+    to_explain = X[[idx]]
 
+    model = shap_dict['MODEL']
 
-    # Get image filename and label
-    img_filename = shap_dict['TEST_SET']['filename'][idx]
-    label = shap_dict['TEST_SET']['label'][idx]
+    # explain how the input to the 7th layer of the model explains the top two classes
+    def map2layer(x, layer):
+        feed_dict = dict(zip([model.layers[0].input], [x.copy()]))
+        return K.get_session().run(model.layers[layer].input, feed_dict)
 
-    # Rearrange prediction probability vector to reflect original ordering of classes in project config
-    probs = [probs[0][shap_dict['CLASSES'].index(c)] for c in shap_dict['TEST_GENERATOR'].class_indices]
+    e = shap.GradientExplainer(
+        (model.layers[layerToExplain].input, model.layers[-1].output),
+        map2layer(X, layerToExplain),
+        local_smoothing=0 # std dev of smoothing noise
+    )
 
-    # Visualize the LIME explanation and optionally save it to disk
-    if save_exp:
-        file_path = shap_dict['IMG_PATH']
-    else:
-        file_path = None
-    if shap_dict['COVID_ONLY'] == True:
-        label_to_see = shap_dict['TEST_GENERATOR'].class_indices['COVID-19']
-    else:
-        label_to_see = 'top'
-    _ = visualize_explanation(orig_img, explanation, img_filename, label, probs, shap_dict['CLASSES'], label_to_see=label_to_see,
-                          dir_path=file_path)
+    shap_values,indexes = e.shap_values(map2layer(to_explain, layerToExplain), ranked_outputs=2)
+
+    # get the names for the classes
+    # Rearrange index vector to reflect original ordering of classes in project config
+    #set_trace()
+    indexes = [[indexes[0][shap_dict['CLASSES'].index(c)] for c in shap_dict['TEST_GENERATOR'].class_indices]]
+    index_names = np.vectorize(lambda x: shap_dict['CLASSES'][x])(indexes)
+
+    # plot the explanations
+    image_plot(shap_values, to_explain, index_names, orig_img=orig_img)
+
     return
-
 
 if __name__ == '__main__':
     shap_dict = setup_shap()
-    i = 0                                                       # Select i'th image in test set
-    explain_xray(shap_dict, i, save_exp=True)                   # Generate explanation for image
+                                        
+    explain_xray_shap(shap_dict, 0, 17, save_exp=True) 
